7005779fa9458e606f9f3d5c436f1900ca8f9530
Maciej <maciej@ogrodniczek.com>
2018-02-05 22:23:43 +0100

    update extractor, add handling for multiple sql files

--- database/generate.sh
+++ database/generate.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-cat "$DIR"/ts3server.sql | sqlite3 ts3server.sqlitedb
+for f in "$DIR"/*.sql
+do
+    echo Creating db from "$f"
+    cat "$f" | sqlite3 ts3server.sqlitedb
+done
